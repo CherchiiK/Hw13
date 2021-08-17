df69100b0c4d6f2eb15258c5f2838680efd16091
CherchiiK <igordudka2033@gmail.com>
2021-08-17 10:28:35 +0300

    Fixed some troubles

--- src/ArifmExpCheck.java
+++ src/ArifmExpCheck.java
@@ -8,14 +8,15 @@ public class ArifmExpCheck {
         secondNumber = new Random().nextInt(a.length);
         System.out.println(firstNumber + " " + secondNumber);
         try {
-            result = firstNumber / secondNumber;
-        } catch (ArithmeticException ex) {
-            firstNumber = new Random().nextInt(a.length);
-            secondNumber = new Random().nextInt(a.length);
+            if (secondNumber == 0.0){
+                throw new DivideByZeroException();
+            }
             result = firstNumber / secondNumber;
         } catch (ArrayIndexOutOfBoundsException ex) {
             firstNumber = new Random().nextInt(a.length);
             secondNumber = new Random().nextInt(a.length);
+        } catch (DivideByZeroException ex) {
+            System.out.println("Divide by zero");
         }
     }
 }
--- src/DivideByZeroException.java
+++ src/DivideByZeroException.java
@@ -1,5 +1,3 @@
 public class DivideByZeroException extends Exception {
-    public DivideByZeroException() {
-        super();
-    }
+
 }
